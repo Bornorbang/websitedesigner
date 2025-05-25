8e7669cc90d3a69c1c00d2336984bc0172c5a14b
bornor <bornorbang@gmail.com>
2025-05-25 05:45:06 -0700

    gj

--- app/signals.py
+++ app/signals.py
@@ -1,26 +1,18 @@
-# blog/signals.py
+# # filepath: c:\Users\WDN\Desktop\websitedesigner\app\signals.py
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+# from django.conf import settings
+# from onesignal_sdk.client import Client
+# from .models import Blog  # Replace with your actual blog post model
 
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import Blog
-import requests
-from django.conf import settings
-
-@receiver(post_save, sender=Blog)
-def send_onesignal_notification(sender, instance, created, **kwargs):
-    if created:
-        url = f"https://websitedesigner.ng{instance.get_absolute_url()}"
-        headers = {
-            "Content-Type": "application/json; charset=utf-8",
-            "Authorization": f"Basic {settings.ONESIGNAL_API_KEY}"
-        }
-
-        payload = {
-            "app_id": settings.ONESIGNAL_APP_ID,
-            "included_segments": ["Subscribed Users"],
-            "headings": {"en": "New Blog Post"},
-            "contents": {"en": instance.title},
-            "url": url
-        }
-
-        requests.post("https://onesignal.com/api/v1/notifications", json=payload, headers=headers)
+# @receiver(post_save, sender=Blog)
+# def send_push_notification(sender, instance, created, **kwargs):
+#     if created:  # Only send notification for new posts
+#         client = Client(app_id=settings.ONESIGNAL_APP_ID, rest_api_key=settings.ONESIGNAL_API_KEY)
+#         notification = {
+#             "headings": {"en": "New Blog Post!"},
+#             "contents": {"en": f"{instance.title} is now live!"},
+#             "included_segments": ["All"]  # Send to all subscribers
+#         }
+#         response = client.send_notification(notification)
+#         print(response)  # Optional: Log the response
